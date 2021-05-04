0c323b067e2650c47c87b97140060f8a10145ade
Steven Jimenez <stevenjimenez2029@gmail.com>
2021-05-04 14:10:41 -0400

    Console App prior to SRP changes.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,35 @@ namespace SingleResponsbilityPrinciple
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to my application!");
+
+            //Ask fo user information
+            Person user = new Person();
+
+            Console.Write("What is your first name: ");
+            user.FirstName = Console.ReadLine();
+
+            Console.Write("What is your last name: ");
+            user.LastName = Console.ReadLine();
+
+            //Checks to be sure if the first and last names are valid
+            if (string.IsNullOrWhiteSpace(user.FirstName))
+            {
+                Console.WriteLine("You did not give us a valid first name!");
+                Console.ReadLine();
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(user.LastName))
+            {
+                Console.WriteLine("You did not give us a valid last name!");
+                Console.ReadLine();
+                return;
+            }
+
+            // Creates a username for the user
+            Console.WriteLine($"Your username is { user.FirstName.Substring(0, 1)}{ user.LastName }");
+
+            Console.ReadLine();
         }
     }
 }
